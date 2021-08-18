a59f0e0c06d4732fa6384686610b5000e4d9235e
micoongkeko <ivongkeko@gmail.com>
2021-08-18 16:54:08 +0800

    Added user model spec and show feature spec

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -8,20 +8,22 @@ RSpec.feature "CreateUsers", type: :system do
 
   before :all do
     User.destroy_all
+    Role.destroy_all
+    @role = Role.create(name: "user")
   end
 
   it "visits the page with the form to create a user and devise" do
     visit 'users/sign_up'
 
-    fill_in "user_name", with: "First"
-    fill_in "user_email", with: "newuser@gmail.com"
-    fill_in "user_balance", with: 10000
-    fill_in "user_role_id", with: 2
-    fill_in "user_password", with: "newuser123"
-    fill_in "user_password_confirmation", with: "newuser123"
+    fill_in "Name", with: "First"
+    fill_in "Email", with: "newuser@gmail.com"
+    fill_in "Balance", with: 10000
+    fill_in "Role", with: @role.id
+    fill_in "Password", with: "newuser123"
+    fill_in "Password confirmation", with: "newuser123"
 
     click_on 'Sign up'
 
-    expect {User.create!}.to change {User.count}.by(1)
+    expect { User.create! }.to change { User.count }.by(1)
   end
 end
